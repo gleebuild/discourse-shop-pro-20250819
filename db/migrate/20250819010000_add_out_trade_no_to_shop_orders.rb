24b7e3f6033aa29d67c63a0bf6a7fe0723917f43
gleebuild <gleebuild@gmail.com>
2025-08-19 02:46:32 +0000

    Delete db directory

--- db/migrate/20250819010000_add_out_trade_no_to_shop_orders.rb
+++
@@ -1,8 +0,0 @@
-
-# frozen_string_literal: true
-class AddOutTradeNoToShopOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :shop_orders, :out_trade_no, :string
-    add_index  :shop_orders, :out_trade_no, unique: true
-  end
-end
